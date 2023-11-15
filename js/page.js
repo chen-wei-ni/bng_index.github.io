57e726897b67ced6710ac014257dfd64f7c7aa69
chen-wei-ni <bcg2135@gmail.com>
2023-11-15 17:30:22 +0800

    gamepage

--- js/page.js
+++ js/page.js
@@ -1,4 +1,5 @@
-const bannerSwiper = new Swiper('.swiper-container', {
+// game頁面 上方banner
+const bannerSwiper = new Swiper('.swiper-container-pc', {
     slidesPerView: 1,
     centeredSlides: false,
     paginationClickable: true,
@@ -15,3 +16,24 @@ const bannerSwiper = new Swiper('.swiper-container', {
     },
 
 });
+const bannerSwiperMb = new Swiper('.swiper-container-mb', {
+    slidesPerView: 1,
+    centeredSlides: false,
+    paginationClickable: true,
+    loop: true,
+    spaceBetween: 0,
+    speed: 2000,
+    autoplay: {
+        reverseDirection: false,
+        disableOnInteraction: false
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+})
+
+//篩選遊戲
+const rad = document.games.Providers;
+const rad2 = document.games.Theme;
+let gs = document.querySelectorAll(".games-area a");
